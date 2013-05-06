732add33da6dc7b0a51db1f1ea984fff79e72ef0
aroemers <github@company.romeinszoon.nl>
2013-05-06 23:36:20 +0200

    Add scala mode.

---
+++ init-commit.el
@@ -0,0 +1,4 @@
+(setq default-major-mode 'text-mode)
+(add-hook 'text-mode-hook 'turn-on-auto-fill)
+(set-fill-column 70)
+(menu-bar-mode -1)
--- init.el
+++ init.el
@@ -34,8 +34,8 @@ Usage: (package-require 'package)"
 
 ;; Initialize installed packages
 (package-initialize)
-;; Package init not needed, since it is done anyway in emacs 24 after reading the init
-;; but we have to load the list of available packages
+;; Package init not needed, since it is done anyway in emacs 24 after reading
+;; the init but we have to load the list of available packages
 (when (not package-archive-contents)
   (package-refresh-contents))
 
@@ -66,13 +66,27 @@ Usage: (package-require 'package)"
 ;;;-----------------------------------------------------------------------------
 
 ;; Require the needed packages.
-(package-require 'paredit)
 (package-require 'clojure-mode)
 (package-require 'nrepl)
 (package-require 'nrepl-ritz)
 
 ;; Make sure paredit is active when clojure mode is active.
+(package-require 'paredit)
 (add-hook 'clojure-mode-hook 'paredit-mode)
+
+
+;;;-----------------------------------------------------------------------------
+;;; Scala mode
+;;;-----------------------------------------------------------------------------
+
+;; Require the needed packages and make sure it is active when a Scala file is
+;; loaded. Note that there is also a Scala-mode2, which can check out some day.
+(package-require 'scala-mode)
+(require 'scala-mode-auto)
+
+;; Make sure paredit is active when Scala mode is active.
+(package-require 'paredit)
+(add-hook 'scala-mode-hook 'paredit-mode)
 
 
 ;;;-----------------------------------------------------------------------------
@@ -99,7 +113,7 @@ Usage: (package-require 'package)"
 
 ;; Use auto-completion in the following modes.
 (dolist (mode '(text-mode html-mode nxml-mode sh-mode clojure-mode
-                          lisp-mode latex-mode))
+                          lisp-mode latex-mode scala-mode))
   (add-to-list 'ac-modes mode))
 
 ;; Map auto-complete to M-TAB, to use if it does not pop up automatically.
