bd7137698be9621d894be0de139dd3b345dfd0af
Arnout Roemers <roemers@zorgdomein.nl>
2020-04-28 11:47:46 +0200

    Fix ido mode not showing everywhere

--- init.el
+++ init.el
@@ -186,6 +186,9 @@
 ;; the ido minibuffer, which comes in handy when selecting a dired
 ;; directory (M-x dired).
 (use-package ido
+  :config
+  (ido-mode t)
+
   :custom
   (ido-enable-flex-matching t)
   (ido-everywhere t))
@@ -446,7 +449,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(package-selected-packages
+   (quote
+    (sbt-mode yaml-mode window-numbering which-key use-package undo-tree smex scala-mode projectile nyan-mode monokai-theme magit lsp-ui ido-vertical-mode ido-completing-read+ hl-todo goto-last-change git-gutter flycheck-joker flx-ido expand-region exec-path-from-shell company-lsp clj-refactor avy auto-indent-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
